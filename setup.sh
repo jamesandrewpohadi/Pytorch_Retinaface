fbbfd0c87bedb91586307086da95dae0a900aff2
jamesandrewpohadi <jamesandrewpohadi@gmail.com>
2020-01-23 17:10:50 +0800

    quite unzip

--- setup.sh
+++ setup.sh
@@ -2,7 +2,7 @@ wget https://f000.backblazeb2.com/file/jamesapohadi-dataset/Resnet50_Final.pth
 mkdir weights
 mv Resnet50_Final.pth weights/
 wget https://f000.backblazeb2.com/file/jamesapohadi-dataset/widerface.zip
-unzip widerface.zip
+unzip -qq widerface.zip
 mv widerface data/
 
 python3 -m pip install python-telegram-bot
